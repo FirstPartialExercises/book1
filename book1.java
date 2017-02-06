6c069bf57a7d389797465d3fdc590fd6154fd8ba
jaime17 <A01411527@itesm.mx>
2017-02-05 20:06:03 -0600

    Create book1.java

---
+++ book1.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstpartialexercises;
+
+/**
+ *
+ * @author jaime
+ */
+public class book1 {
+    //Declaration of atributes
+    private String name;
+    private Author author;
+    private double price;
+    private int qty=0;
+    //Initialize atributes
+    public book1(String name, Author author, double price){
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+    public book1(String name, Author author, double price, int qty){
+        this.name=name;
+        this.author = author;
+        this.price=price;
+        this.qty=qty;
+    }
+    //Getters and Setters
+    public String getName(){
+        return this.name;
+    }
+    public Author getAuthor(){
+        return this.author;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public int getQty(){
+        return this.qty;
+    }
+    public void setQty(){
+        this.qty = qty;
+    }
+    @Override
+    public String toString(){ //return Strings
+        return "Book[name = " + this.name + "Author[" + author.toString() + "]" + ", price = " + this.price + ", qty = " + this.qty + "]";
+    }
+}
+
